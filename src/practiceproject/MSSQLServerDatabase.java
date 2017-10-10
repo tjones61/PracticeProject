36de19671e1b1100a927fe1169798d54f3646a58
tjones61 <tjones61@10.18.12.31>
2017-10-10 15:44:42 -0500

    addition of classes

--- src/practiceproject/MSSQLServerDatabase.java
+++ src/practiceproject/MSSQLServerDatabase.java
@@ -9,13 +9,6 @@ package practiceproject;
  *
  * @author tjones61
  */
-public class PracticeProject {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class MSSQLServerDatabase {
     
 }
